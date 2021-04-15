7126bb4ab68ae2f3078f362e6c84be1172616ecb
cloudsoldier <36954152+cloudsoldier@users.noreply.github.com>
2021-04-15 23:00:28 +0100

    Add files via upload

---
+++ lab9.3/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+resource_group_name  = "Terra-rg"
+storage_account_name = "remotesa10"
+container_name       = "lab9-3"
+key                  = "Lab9.3.tfstate"
+}
+ }
+
+ 
