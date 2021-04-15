5dddc42edbc1394f4351b80dee5a7d67588525a5
cloudsoldier <36954152+cloudsoldier@users.noreply.github.com>
2021-04-15 22:57:13 +0100

    Delete backend.tf

--- Sec09/lab9.3/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-resource_group_name  = "Terra-rg"
-storage_account_name = "remotesa10"
-container_name       = "lab9"
-key                  = "Lab9.3.tfstate"
-}
- }
